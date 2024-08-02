9db1fe64b74a6a8a34edb45da090edf4c8f2b905
Morante <112764388+matheusmorante@users.noreply.github.com>
2024-08-02 17:40:04 -0300

    Delete src directory

--- src/load_data.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Análise de Vendas").getOrCreate()
-
-sales_df = spark.read.csv("hdfs:///user/hadoop/vendas.csv", header=True, inferSchema=True)
-products_df = spark.read.csv("hdfs:///user/hadoop/produtos.csv", header=True, inferSchema=True)
-
-vendas_df.show()
-produtos_df.show()
